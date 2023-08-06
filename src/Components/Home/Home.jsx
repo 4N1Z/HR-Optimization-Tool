a131302d924b47af1f441356236a92245e0bd48b
4N1Z <20am014@sctce.ac.in>
2023-08-06 16:42:53 +0530

    changes

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+
+        <div className="navbar">
+            <img src="" alt="" className="logo" />
+            <div className="homeBtn">
+                <button className="btn">Home</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
