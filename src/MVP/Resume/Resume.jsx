a374625416d61d2f8195d709facce7842e1b6408
aswnss_ubuntu <aswinlalsct@gmail.com>
2023-08-08 15:34:51 +0530

    working on file upload

--- src/MVP/Resume/Resume.jsx
+++ src/MVP/Resume/Resume.jsx
@@ -1,71 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import { tick } from '../../assets/export';
 import { Link } from 'react-router-dom';
 import Navbar from '../../Components/Navbar/Navbar';
 import "./Resume.css"
 function Resume() {
 
-  const [email, setEmail] = useState('');
+    const [resume, setResume] = useState(null);
+    const [jobDesc, setJobDesc] = useState(null);
+    const fileUpload = (i) => {
+        // Create a hidden file input element
+        var fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.style.display = 'none';
 
-  const fileUpload =()=>{
-    // Create a hidden file input element
-    var fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.style.display = 'none';
-  
-    // Trigger the file input click event
-    fileInput.click();
-  
-    // Listen for file selection
-    fileInput.addEventListener('change', function() {
-      var selectedFile = fileInput.files[0];
-      // Do something with the selected file
-      console.log('Selected file:', selectedFile);
-    });
-  }
+        // Trigger the file input click event
+        fileInput.click();
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+        // Listen for file selection
+        fileInput.addEventListener('change', (i) => {
+            var selectedFile = fileInput.files[0];
+            console.log(selectedFile);
+            if (i === 1) {
+                setResume(selectedFile);
+                console.log("I am working");
+            }
+            else {
+                setJobDesc(selectedFile);
+            }
+        });
+    }
 
-  const handleSubmit = (event) => {
-    event.preventD
-    efault();
-    // You can perform further actions here, such as sending the email to a server.
-    console.log(`Subscribing email: ${email}`);
-  };
-  return (
 
-    <div>
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // You can perform further actions here, such as sending the email to a server.
+    };
 
-        <Navbar />
-      <div className="HomeContainer">
+    useEffect(() => {
+        // remove passive class from button when resume is uploaded
+        if (resume) {
+            document.getElementById('jobDescBtn').classList.remove('passive');
+            document.getElementById('jobDescBtn').disabled = false;
 
-        <div className="textContainer animate-fade-in-top-to-bottom">
-          <h5 className="topTitle">HR OPTIMIZATION TOOl</h5>
-          <h1 className="mainTitle">Rank resume against a job description</h1>
-          <h4 className="subTitle">Do you want resumes scored against job description and send custom mails to the recruiter highlighting applicants proficient fields and score ?</h4>
+        }
+    }, [resume]);
+    return (
+
+        <div>
+
+            <Navbar />
+            <div className="HomeContainer">
+
+                <div className="textContainer animate-fade-in-top-to-bottom">
+                    <h5 className="topTitle">HR OPTIMIZATION TOOl</h5>
+                    <h1 className="mainTitle">Rank resume against a job description</h1>
+                    <h4 className="subTitle">Do you want resumes scored against job description and send custom mails to the recruiter highlighting applicants proficient fields and score ?</h4>
+                </div>
+
+                <form className="formStyle animate-fade-in" onSubmit={handleSubmit}>
+                    <div className="sendMailWrapper">
+                        <button onClick={() => { fileUpload(1) }} className='smallBtnStyle Btn'>Upload Resume</button>
+                        <button onClick={() => { fileUpload(2) }} className='smallBtnStyle Btn passive' id="jobDescBtn">Upload Job Description</button>
+                        <button className='smallBtnStyle Btn passive' type='submit'>Analyse</button>
+
+                    </div>
+                </form>
+
+                <div className="disclaimerTextBox animate-fade-in">
+                    <p className="disclaimerText"><span className='giveColor'>.docx</span> or <span className='giveColor'>.pdf</span> format files required</p>
+                </div>
+                <div className="line"></div>
+                <div className='sendMailBox'>
+                    <h1></h1>
+                </div>
+            </div>
+
         </div>
-
-        <form className="formStyle animate-fade-in" onSubmit={handleSubmit}>
-          <div className="sendMailWrapper">           
-          <button onClick={fileUpload} className='smallBtnStyle Btn'>Upload Resume</button>
-          <button onClick={fileUpload} className='smallBtnStyle Btn'>Upload Job Description</button>
-          </div>
-        </form>
-
-        <div className="disclaimerTextBox animate-fade-in">
-          <img src={tick} width = "18px" alt="tick"/>
-          <p className="disclaimerText">It's 100% free and we will send more than one email per month</p>
-        </div>
-        <div className="line"></div>
-        <div className='sendMailBox'>
-            <h1></h1>
-        </div>
-      </div>
-
-    </div>
-  )
+    )
 }
 
 export default Resume
