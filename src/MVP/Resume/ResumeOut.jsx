629cc90c34e3fe7aa36052690413a92658a34a60
4N1Z <20am014@sctce.ac.in>
2023-08-10 15:59:21 +0530

    resume out page

---
+++ src/MVP/Resume/ResumeOut.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function ResumeOut() {
+    return (
+        <div>
+            <Navbar name="back" link="/description_ranker" />
+            <div className='HomeContainer'>
+                <div className="outputContainer">
+                    <h2 className="titleOut">Result of Analysis</h2>
+                    <div className="firstBox">
+                        <h2>Here is the description after analysis</h2>
+                        <div className="contentBox">
+                            <div className="parent contentInsideBox">
+                                <div className="child1 itemBox">
+                                    <h1>NAME</h1>
+                                    <p>John Doe</p>
+                                </div>
+                                <div className="child2 itemBox">
+                                    <h1>NAME</h1>
+                                    <p>John Doe</p>
+                                </div>
+                                <div className="child3 itemBox">
+                                    <h1>NAME</h1>
+                                    <p>John Doe</p>
+                                </div>
+                                <div className="child4 itemBox">
+                                    <h1>Desciption</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros
+                                        elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+                                </div>
+                            </div>
+
+                            <Link to='/resume_ranker'>
+                                <button className="goToResume Btn"> <p>Correct</p> </button>
+                            </Link>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ResumeOut
